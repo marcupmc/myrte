caccd6196a6aa27749e8de1905c23b4ba1b728d8
Marc Gregoire <marc.gregoire@tinyclues.com>
2016-11-01 15:02:49 +0100

    oups

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
 console.log('----Hello World-----');
 require ('../assets/styles/global.scss');
 require ('../assets/styles/components/nav-bar.scss');
-require('../assets/styles/pages/home.scss');
+require('../assets/styles/pages/home.scss'); 
