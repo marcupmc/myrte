529074f2e2f80162c3aa521bb149bff8d306044e
Marc Gregoire <marc.gregoire@tinyclues.com>
2017-01-24 07:58:48 +0100

    force

--- app/actions/products.js
+++ app/actions/products.js
@@ -3,5 +3,5 @@ import productsActions from '../constants/productsActions';
 export function fetchProducts() {
     return {
         type: productsActions.FETCH_PRODUCTS_START,
-    };
+    }; //TODO
 }
